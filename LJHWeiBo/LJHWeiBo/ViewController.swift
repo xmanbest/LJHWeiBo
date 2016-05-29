81e1d8103d422d19a6b229b35d664792cd4365c1
xmanbest <lijianhuax@icloud.com>
2016-05-29 21:11:58 +0900

    1

---
+++ LJHWeiBo/LJHWeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LJHWeiBo
+//
+//  Created by 李建華 on 16/5/29.
+//  Copyright © 2016年 lijianhua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
